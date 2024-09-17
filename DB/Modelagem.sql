beb3d56b64ef158164554e7169b0d505e7262730
EmilianoJMC <86630896+EmilianoJMC@users.noreply.github.com>
2024-09-17 17:58:09 -0300

    Revert "teste"
    This reverts commit ed497d95a88e56c270d3ba38f99824e89440a3e8.

--- DB/Modelagem.sql
+++ DB/Modelagem.sql
@@ -11,13 +11,5 @@ CREATE TABLE `stock_prices` (
   `low` text,
   `close` text,
   `volume` text,
-  `symbol` varchar(10) DEFAULT NULL,
-  UNIQUE KEY `pk_stock` (`timestamp`,`symbol`)
+  `symbol` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- Alterado coluna symbol de texto para Varchar
-ALTER TABLE stock_prices MODIFY symbol VARCHAR(10);
-
--- Criado Primary Key para colunas timestamp e symbol
-ALTER TABLE stock_prices ADD CONSTRAINT pk_stock UNIQUE (timestamp, symbol);
